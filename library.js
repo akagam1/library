ed528ff69345fcfdcc82157923aa48a993227c23
arjun <arjunputhli00@gmail.com>
2022-01-09 13:07:07 +0530

    layout complete

--- library.js
+++ library.js
@@ -1,14 +1,19 @@
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+class Book {
+    constructor(title, author, pages, readStatus){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = readStatus;
+    }
 }
 
-function addBookToLibrary() {
-    const book = new Book(title, author, pages, status);
+function addBook(title, author, pages, readStatus) {
+    const book = new Book(title, author, pages, readStatus);
     myLibrary.push(book);
 }
 
+//Associate each DOM element with index
+//When removing book we can access the index attribute and pop it from the array
+//Then call function and re-draw the book cards on the screen by looping through the array
