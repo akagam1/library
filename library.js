560f1590d142fd12a35e4c55d93cb3a0e0e3bc98
arjun <arjunputhli00@gmail.com>
2022-01-21 10:35:13 +0530

    prevented defualt button behavior

--- library.js
+++ library.js
@@ -15,7 +15,13 @@ function addBook(title, author, pages, readStatus) {
     myLibrary.push(book);
 }
 
-const bookForm = document.getElementById("forms");
+let bookForm = document.getElementById("forms");
+    bookForm.addEventListener("keypress", function(event){
+      if (event.key === "Enter") {
+        event.preventDefault();
+      }
+});
+
 //Associate each DOM element with index
 //When removing book we can access the index attribute and pop it from the array
 //Then call function and re-draw the book cards on the screen by looping through the array
