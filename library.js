1b8414e02cd2dc543069abbfe7ce6eb8687d48fd
arjun <arjunputhli00@gmail.com>
2022-01-25 11:13:15 +0530

    completed add book function

--- library.js
+++ library.js
@@ -1,26 +1,39 @@
 let myLibrary = [];
-
-
-class Book {
-    constructor(title, author, pages, readStatus){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.status = readStatus;
-    }
-}
-
-function addBook(title, author, pages, readStatus) {
-    const book = new Book(title, author, pages, readStatus);
-    myLibrary.push(book);
-}
+let cardContainer = document.getElementById("cardContainer");
 
 let bookForm = document.getElementById("forms");
-    bookForm.addEventListener("keypress", function(event){
+bookForm.addEventListener("keypress", function(event){
       if (event.key === "Enter") {
         event.preventDefault();
       }
 });
+
+class Book {
+    constructor(title, author, pages, element, readStatus='no'){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = readStatus;
+        this.element = element
+    }
+}
+let bookTitle = document.getElementById("title");
+let authName = document.getElementById("authName");
+let numPages = document.getElementById("numPages");
+
+function addBook() {
+    bookForm.onsubmit = function(){
+        let card = document.createElement('div');
+        card.classList.add("card")
+        cardContainer.appendChild(card);
+        let book = new Book(bookTitle.value, authName.value, numPages.value, card);
+        myLibrary.push(book);
+    }
+}
+
+function displayBook() {
+    
+}
 
 //Associate each DOM element with index
 //When removing book we can access the index attribute and pop it from the array
