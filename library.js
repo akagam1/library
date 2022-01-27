ec202c952f1a3ebb012bc7a2e87c4c9d100cffc7
arjun <arjunputhli00@gmail.com>
2022-01-27 12:43:40 +0530

    card layout complete

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 let cardContainer = document.getElementById("cardContainer");
+let button = document.getElementById("but");
 
 let bookForm = document.getElementById("forms");
 bookForm.addEventListener("keypress", function(event){
@@ -8,8 +9,13 @@ bookForm.addEventListener("keypress", function(event){
       }
 });
 
+bookForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    addBook();
+});
+
 class Book {
-    constructor(title, author, pages, element, readStatus='no'){
+    constructor(title=0, author=0, pages=0, element, readStatus='no'){
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -20,19 +26,20 @@ class Book {
 let bookTitle = document.getElementById("title");
 let authName = document.getElementById("authName");
 let numPages = document.getElementById("numPages");
+bookForm.addEventListener('submit', addBook());
 
 function addBook() {
-    bookForm.onsubmit = function(){
+    /*bookForm.onsubmit = function(){*/
         let card = document.createElement('div');
-        card.classList.add("card")
-        cardContainer.appendChild(card);
+        card.classList.add("card");
         let book = new Book(bookTitle.value, authName.value, numPages.value, card);
-        myLibrary.push(book);
-    }
-}
-
-function displayBook() {
-    
+        myLibrary.push(book); 
+        let i = myLibrary.length - 1;     
+        myLibrary[i].element.style.backgroundColor = "#3882f6"; 
+        myLibrary[i].element.style.height = "350px";
+        myLibrary[i].element.style.width = "250px";   
+        myLibrary[i].element.style.margin = "50px";
+        cardContainer.appendChild(myLibrary[i].element);  
 }
 
 //Associate each DOM element with index
